bd06a35b7dadb29ed7511034a1d7937d869e816c
Mouctar KABA <mocktarkba@gmail.com>
2025-05-07 11:07:40 +0000

    modif

--- grails-app/domain/gestionetudiants/Etudiant.groovy
+++ grails-app/domain/gestionetudiants/Etudiant.groovy
@@ -3,11 +3,13 @@ package gestionetudiants
 class Etudiant {
 
     String nom
+    String dnom
     String prenom
     String email
     Date dateNaissance
 
     static constraints = {
+        dnom black: false
         nom blank: false
         prenom blank: false
         email blank: false, email: true
